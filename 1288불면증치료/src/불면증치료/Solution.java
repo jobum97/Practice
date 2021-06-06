05b8b8b444f72ef3ffff9bf369ce4598dc25815f
jobum97 <brian654321@naver.com>
2021-06-06 14:38:23 +0900

    SW_D2

---
+++ 1288불면증치료/src/불면증치료/Solution.java
@@ -0,0 +1,33 @@
+package 불면증치료;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static BufferedReader input =new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output =new StringBuilder();
+	static String src="5\r\n"
+			+ "1\r\n"
+			+ "2\r\n"
+			+ "11\r\n"
+			+ "1295\r\n"
+			+ "1692";
+	
+	public static void main(String args[]) throws IOException {
+		//input= new BufferedReader(new InputStreamReader(System.in));
+		input=new BufferedReader(new StringReader(src));
+		int testcase=Integer.parseInt(input.readLine());
+		
+ 		for(int t=1;t<=testcase;t++) {
+ 			StringTokenizer str;
+ 			str=new StringTokenizer(input.readLine());
+ 		
+ 		
+ 		
+ 			
+ 		}
+	}	
+}
