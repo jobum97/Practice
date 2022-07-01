78759c8791920e9c8c61f0931d2fd84dcdfb308e
jobum97 <brian654321@naver.com>
2022-07-01 18:28:58 +0900

    백준

--- 2470두 용액/Main.java
+++ 2470두 용액/Main.java
@@ -26,49 +26,30 @@ public class Main {
         Arrays.sort(data);
        // System.out.println(Arrays.toString(data));
 
-        int sanStart = -1;
-        int alStart = 0;
-        for (int i = 0; i < N; i++) {
-            if (data[i] > 0) {
-                sanStart = i;
-                break;
+        int i = 0;
+        int j = data.length - 1;
+
+        int gap = Integer.MAX_VALUE;
+        int ans1 = 0;
+        int ans2 = 0;
+
+        int temp;
+        int sum;
+        while (i < j) {
+            sum = data[i] + data[j];
+            temp = Math.abs(sum);
+            if (temp < gap) {
+                gap = temp;
+                ans1 = data[i];
+                ans2 = data[j];
             }
+            if (sum > 0)
+                j--;
+            else
+                i++;
         }
 
-        if (sanStart == 0 || sanStart == -1) {
-            //산성만 잇거나 알칼리만 있는 경우
-            if (data[0] < 0) {
-                System.out.println(data[N - 2] + " " + data[N - 1]);
-            }else{
-                System.out.println(data[0] + " " + data[1]);
-            }
-        }else{
-            int minSum = Integer.MAX_VALUE;
-            int minAl = 0, minSan = 0;
-            alStart = sanStart - 1;
-            while (alStart >= 0 && sanStart < N) {
-                int sum = data[alStart] + data[sanStart];
-                //System.out.println(data[alStart] + " " + data[sanStart]);
-                //최소 갱신
-                if (minSum > Math.abs(sum)) {
-                    minAl = data[alStart];
-                    minSan = data[sanStart];
-                    minSum = Math.abs(sum);
-                }
-
-                //산이 더 강한경우
-                if (sum > 0) {
-                    alStart--;
-                } else if (sum == 0) {
-                    System.out.println(data[alStart] + " " + data[sanStart]);
-                    return;
-                }else{
-                    sanStart++;
-                }
-            }
-            System.out.println(minAl + " " + minSan);
-
-        }
+        System.out.println(ans1 + " " + ans2);
 
     }
 }
