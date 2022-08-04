7baed022612fedfa70d628c451847e072cd701e0
jobum97 <brian654321@naver.com>
2022-08-04 19:44:20 +0900

    백준

--- 1019책 페이지/Main.java
+++ 1019책 페이지/Main.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 public class Main {
     static BufferedReader input;
     static StringBuilder output = new StringBuilder();
-    static String src = "230";
+    static String src = "50";
     static int counts[] = new int[10];
-    static int CT = 1;
+    static int CT = 1; // count 자릿수
 
 
     public static void main(String[] args) throws IOException {
@@ -18,7 +18,8 @@ public class Main {
         int N = Integer.parseInt(input.readLine());
         int A = 1;
         while (A <= N) {
-            //System.out.println(A + " " + N);
+
+            // N은 일의 자리 9, A는 일의 자리 0이 되게 조정하면서 일의 자리수 count
             while (N % 10 != 9 && A <= N) {
                 count(N);
                 N--;
@@ -32,6 +33,8 @@ public class Main {
             }
             A /= 10;
             N /= 10;
+
+            // A(aa0) ~ N(nn9) 자릿수 계산
             for (int i = 0; i < 10; i++) {
                 counts[i] += (N - A + 1) * CT;
             }
