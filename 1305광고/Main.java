0f73a4dd5df8ab6c68e7ee1e75aa610c37197405
jobum97 <brian654321@naver.com>
2022-08-21 15:57:34 +0900

    백준

--- 1305광고/Main.java
+++ 1305광고/Main.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Main {
     static BufferedReader input;
     static StringBuilder output = new StringBuilder();
-    static String src = "5\n" +
-            "aaaaa";
+    static String src = "6\n" +
+            "aabaaa";
 
-    static int L;
+    static int L, pi[];
     static String snapshot;
     public static void main(String[] args) throws IOException {
 
@@ -18,7 +18,21 @@ public class Main {
 
         // 보인 문자열을 반복으로 만들 수 있는 문자열 중 가장 짧은 문자열
 
+        // 가능할 수 있는 풀이
+        pi = new int[L];
+        getPi();
+        System.out.println(L - pi[L - 1]);
+    }
 
-
+    public static void getPi(){
+        int j = 0;
+        for (int i = 1; i < L; i++) {
+            while (j > 0 && snapshot.charAt(i) != snapshot.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (snapshot.charAt(i) == snapshot.charAt(j)) {
+                pi[i] = ++j;
+            }
+        }
     }
 }
