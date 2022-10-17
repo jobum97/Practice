0583f6edf4bca60ab967443b49ad01d3d05fea5a
jobum97 <brian654321@naver.com>
2022-10-17 22:55:25 +0900

    프로그래머스

--- 22nc-3/Main.java
+++ 22nc-3/Main.java
@@ -1,19 +1,4 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class Main {
     public static void main(String[] args) {
-        // DB 쿼리 문제
-
-
-
-        int[][] data = new int[10][10];
-        Arrays.sort(data, new Comparator<int[]>() {
-            public int compare(int[] o1, int[] o2) {
-                return o1[1] - o2[1];
-            }
-        });
     }
-
-
 }
