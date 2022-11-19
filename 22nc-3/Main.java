f9c9f6740e1a5e261fab3e1f3c9d38586ff0ee78
jobum97 <brian654321@naver.com>
2022-11-19 21:03:01 +0900

    fix

--- 22nc-3/Main.java
+++ 22nc-3/Main.java
@@ -3,67 +3,114 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(new int[][]{{0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 1, 0, 0}, {0, 0, 0, 1, 0, 1, 0, 0}, {0, 1, 1, 0, 0, 1, 0, 0}, {0, 0, 0, 1, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 0, 0, 1}}, new int[]{0, 0}, new int[]{6, 3}));
+        System.out.println(Arrays.toString(solution.solution(new int[][]{{0, 2, 3, 1}, {5, 3, 3, 1}, {10, 2, 4, 1}})));
+
     }
 }
 
-
 class Solution {
+    public int[] solution(int[][] jobs) {
+        ArrayList<Integer> answerArr = new ArrayList<>();
 
-    public int solution(int[][] M, int[] S, int[] D) {
-        int answer = -1;
-        int n = M.length;
-        int m = M[0].length;
-        int moveSet[][] = {{-1,0},{1,0},{0,1},{0,-1}};
-        boolean[][] checked = new boolean[n][m];
+        // 한번 작업 시작하면 해당 분류 작업들 마무리까지함 요청은 받을 수 잇음
+        // 중요도 합이 높은 부류 처리, 같다면 분류 번호 오름차순으로 처리
 
-        System.out.println(n+" "+m);
+        // 분류 같다면 사실상 같은 작업으로 봐도 되는 부분
 
-        PriorityQueue<Coord> pq = new PriorityQueue<>();
-        pq.add(new Coord(S[0],S[1],0));
-        checked[S[0]][S[1]] = true;
+        // 작업 : 분류번호, 중요도 합, 시간 합
+        int finish =0;
 
-        while(!pq.isEmpty()){
-            Coord cur = pq.poll();
-            System.out.println(cur.row + " " + cur.col);
-            if(cur.row == D[1] && cur.col == D[0]){
-                answer = cur.dist;
-                break;
-            }
 
-            for(int dir=0; dir<moveSet.length;dir++){
-                int nextRow = cur.row +moveSet[dir][0];
-                int nextCol = cur.col +moveSet[dir][1];
+        int[][] taskStatus = new int[101][3];
+        boolean[] checked = new boolean[jobs.length];
+        int cursor = jobs[0][2];
+        answerArr.add(cursor);
+        int timeLimit = jobs[0][0]+jobs[0][1];
+        checked[0] = true;
+        finish++;
+        int idx =1;
 
-                if(nextCol<0 || nextRow < 0 || nextCol>=m || nextRow >=n){
-                    continue;
+        int Do =0;
+        // 작업: 요청시각, 소요시간, 분류번호, 중요도
+        while(Do<5){
+            // 현재 작업 기준으로 되는대로 분류 번호 쭉쭉 처리
+            for(int i=idx;i<jobs.length;i++){
+
+                System.out.println(Arrays.toString(jobs[i])+" "+timeLimit+" "+finish);
+                // 현재 작업과 같은 분류이고 시간 제한안에 들어온 요청이면 이어서 동작
+                if(!checked[i] && cursor == jobs[i][2]  && jobs[i][0] <= timeLimit){
+                    checked[i] = true;
+                    timeLimit += jobs[i][1];
+                    finish++;
                 }
-                System.out.println("-" + nextRow + " " + nextCol);
-                if(!checked[nextRow][nextCol] && M[nextRow][nextCol] ==0){
-                    checked[nextRow][nextCol] = true;
-                    pq.add(new Coord(nextRow,nextCol,cur.dist+1));
+
+                // 다르다면 쌓아두자
+                if(!checked[i] && cursor != jobs[i][2]  && jobs[i][0] <= timeLimit){
+                    taskStatus[jobs[i][2]][0] += jobs[i][1];
+                    taskStatus[jobs[i][2]][1] += jobs[i][3];
+                    taskStatus[jobs[i][2]][2]++;
+                    checked[i] = true;
+                }
+
+                // 시간 초과시 해당 분류 작업 끝
+                if(timeLimit < jobs[i][0]){
+                    idx =i;
+                    break;
                 }
             }
-
+            System.out.println(timeLimit+" "+idx+" "+cursor);
+            // 끝난 시간 기점으로 판단
+            for(int i=1;i<5;i++){
+                System.out.println(Arrays.toString(taskStatus[i]));
+            }
+            cursor = getNextWork(taskStatus);
+            // 새로운 작업 착수
+            timeLimit+= taskStatus[cursor][0];
+            finish += taskStatus[cursor][2];
+            taskStatus[cursor][0] =0;
+            taskStatus[cursor][1] =0;
+            taskStatus[cursor][2] =0;
+            System.out.println(cursor+" "+finish);
+            answerArr.add(cursor);
+            Do++;
         }
 
+        int[] answer = new int[answerArr.size()];
+        for(int i=0;i<answerArr.size();i++){
+            answer[i] = answerArr.get(i);
+        }
         return answer;
     }
 
-    public class Coord implements Comparable<Coord>{
-        int row;
-        int col;
-        int dist;
-        Coord(int row, int col, int dist){
-            this.row = row;
-            this.col = col;
-            this.dist = dist;
+    public int getNextWork(int[][] taskStatus){
+        // 중요도 합이 높은 부류 처리, 같다면 분류 번호 오름차순으로 처리
+        int value =0;
+        int idx =0;
+        for(int i=0;i<taskStatus.length;i++){
+            if(value < taskStatus[i][1]){
+                value = taskStatus[i][1];
+                idx =i;
+            }
         }
 
-        public int compareTo(Coord o){
-            return this.dist - o.dist;
-        }
+        return idx;
     }
+
+    // public class Task implements Comparable<Task>{
+    //     int number;
+    //     int cost;
+    //     int value;
+    //     public Task(int number, int cost, int value){
+    //         this.number = number;
+    //         this.cost = cost;
+    //         this.value = value;
+    //     }
+    //     public void add(Task o){
+    //         this.cost +=o.cost;
+    //         this.value +=
+    //     }
+
+    // }
 }
 
 
